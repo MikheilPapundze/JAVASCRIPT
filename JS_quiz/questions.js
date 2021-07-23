ec342ea6f3efba9c3fde3e0d282ced5ec91da9f8
MikheilPapundze <55429620+MikheilPapundze@users.noreply.github.com>
2021-07-23 22:12:26 +0400

    Add files via upload

---
+++ JS_quiz/questions.js
@@ -0,0 +1,431 @@
+    //Questions and answers list
+   let Questions = [
+    {
+      question: "1. When you are in a community (at a party with fellow workers   fellow practitioners), you usually prefer",
+      answer: ["A) participate in a general conversation ", "B) talking with each separately",],
+      answered:false,
+    },
+    {
+      question: "2. You consider yourself more ",
+      answer: ["A) realistic  ", "B) prone to theorizing"],
+      answered:false,
+    },
+    {
+      question: "3. In your opinion, which one is worse?",
+      answer: ["A) 'living in a dream world'  ", "B) 'all goes according to the plan'"], answered:false,  
+    },
+    {
+      question: "4. You are more impressed by",
+      answer: ["A) solid principles  ", "B) strong emotions"],answered:false,
+    },
+    {
+      question: "5. You are more attracted  ",
+      answer: ["A) persuasive   ", "B) sentimental "],answered:false,
+    },
+    {
+      question: "6. If you have to do an unusual job, it's more convenient for you. ",
+      answer: ["A) to plan it in advance   ", "B) to find out what to do, already in the course of work "],answered:false,
+    },
+   {
+      question: "7. When you need to make a choice  make a decision, you make it. ",
+      answer: ["A) mostly attentive and carefully  ", "B) often spontaneously"],answered:false,
+    },
+    {
+      question: "8. At parties  social gatherings you usually ",
+      answer: ["A) stay up late", " B) leave early feeling tired"],answered:false,
+    },
+    {
+      question: "9. You are more attracted to",
+      answer: ["A) realists ", " B) imaginative people",],answered:false,
+    },
+    {
+        question: "10. You are more interested in",
+        answer: ["A) what really exists ", " B) unrealized opportunities"],answered:false,      },
+      {
+        question: "11. Your conclusions about people based ",
+        answer: ["A) on rules more often than on circumstances ", "B) on circumstances more often than on rules"],answered:false,
+      },
+      {
+        question: "12. When assessing other people, you usually",
+        answer: ["A) firm and objective", "B) condescending and subjective"],answered:false,
+      },
+      {
+        question: "13. More often you act",
+        answer: ["A) punctually ", "B) slowly"],answered:false,
+      },
+      {
+        question: "14. You prefer",
+        answer: [" A) doing work in advance ", "B) doing everything last moment"],answered:false,
+      },
+      {
+        question: "15. Among your friends you",
+        answer: [" A) you know everything about everyone ", " B) the last to know what is happening"],answered:false,
+      },
+      {
+        question: "16. When doing your usual work you prefer",
+        answer: ["A) doing it the usual way ", "B) inventing your own way"],answered:false,
+      },
+      {
+        question: "17. When you read at your pleasure, you prefeer when a writer",
+        answer: ["A) clearly explains what he means ", "B) sets out thoughts in an unusual, original form."],answered:false,
+      },
+      {
+        question: "18. What attracts you more",
+        answer: ["A) consistency and reasoning ", "B) the harmony of human relations"],answered:false,
+      },
+      {
+        question: "19. It is easier for you to make judgments.",
+        answer: ["A) based on logic and on facts ", " B) based on your values"],answered:false,
+      },
+      {
+        question: "20. You are more attracted to situations",
+        answer: ["A) in which there is an element of certainty", "B) which are full of unpredictability"],answered:false,
+      },
+      {
+        question: "21. You can say that you are more ",
+        answer: ["A) serious and focused", "B) not taking life too seriously"],answered:false,
+      },
+      {
+        question: "22. When talking on the phone",
+        answer: ["A) you rarely think about: 'What to say?'' in advance ", "B) often think out in advance what you will say"],answered:false,
+      },
+      {
+        question: "23. How do you think? Data",
+        answer: [" A) speak for itself ", " B) illustrates some patterns"],answered:false,
+      },
+      {
+        question: "24. Dreamers ",
+        answer: ["A) annoy you ", "B) attract and fascinate you"],answered:false,
+      },
+      {
+        question: "25. More often you are",
+        answer: ["A) calm and impartial ", "B) cordial and sympathetic"],answered:false,
+      },
+      {
+        question: "26. You think that it is worse to",
+        answer: ["A) be unreasonable ", "B) be malevolent"],answered:false,
+      },
+      {
+        question: "27. In most cases it is better to",
+        answer: ["A) try to control the course of events ", "B) rely on the natural course of events."],answered:false,
+      },
+      {
+        question: "28. You feel better when",
+        answer: ["A) you have already made a purchase ", "B) you still have the opportunity to do it."],answered:false,
+      },
+      {
+        question: "29. In the group of colleagues you tend to",
+        answer: ["A) initiate a conversation ", "B) wait for someone to contact you"],answered:false,
+      },
+      {
+        question: "30. Claims made on the basis of common sense",
+        answer: ["A) rarely raise doubts ", "B) often raise doubts."],answered:false,
+      },
+      {
+        question: "31. Would you like your friend to be a person",
+        answer: ["A) who stands firmly on the ground ", "B) who always has new ideas."],answered:false,
+      },
+      {
+        question: "32. When making decisions it is easier for you to follow",
+        answer: ["A) the rules and standards ", "B) the wishes of interested parties"],answered:false,
+      },
+      {
+        question: "33. You are rather a person",
+        answer: ["A) hard than soft ", "B) soft than hard"],answered:false,
+      },
+      {
+        question: "34. You are more attracted by the opportunity",
+        answer: ["A) to organize something ", "B) creatively use the available opportunities"],answered:false,
+      },
+      {
+        question: "35. In a situation you value",
+        answer: ["A) clarity ", "B) the possibility of unpredictable turns."],answered:false,
+      },
+      {
+        question: "36. Meeting new and unusual people.",
+        answer: ["A) stimulates and energizes you ", "B) tires you"],answered:false,
+      },
+      {
+        question: "37. In most cases you are",
+        answer: ["A) practical ", "B) a dreamer "],answered:false,
+      },
+      {
+        question: "38. You are more interested to know",
+        answer: ["A) wheather other people mayt be helpful to you ", "B) other people's point of view"],answered:false,
+      },
+      {
+        question: "39. You receive more satisfaction ",
+        answer: ["A) through a discussion of the issue ", "B) through reaching agreement on the subject matter."],answered:false,
+      },
+      {
+        question: "40. Your actions are more governed by",
+        answer: ["A) your head ", "B) your heart"],answered:false,
+      },
+      {
+        question: "41. When you know that at a certain time you will be engaged in a certain business,",
+        answer: ["A) you are glad that you can plan your time ", "B) you don't like when you have to follow a certain plan"],answered:false,
+      },
+      {
+        question: "42. Usually you",
+        answer: ["A) strive to achieve desired results ", "B) are satisfied with what comes out"],answered:false,
+      },
+      {
+        question: "43. Do you prefer to have",
+        answer: ["A) a wide circle of communication, consisting of many friends and acquaintances ", "B) a narrow social circle consisting of several friends"],answered:false,
+      },
+      {
+        question: "44. You are more guided by",
+        answer: ["A) facts ", "B) regularities"],answered:false,
+      },
+      {
+        question: "45. You are more interested or would be interested in",
+        answer: ["A) production and distribution of products ", "B) research and design"],answered:false,
+      },
+      {
+        question: "46. Do you consider it a compliment if you are called",
+        answer: ["A) a logical person ", "B) the feeling person"],answered:false,
+      },
+      {
+        question: "47. In yourself you value more",
+        answer: ["A) decisiveness ", "B) devotion"],answered:false,
+      },
+      {
+        question: "48. When you express your thoughts or judgments, you prefer them to be",
+        answer: ["A) completed ", " B) preliminary with the possibility of improvement"],answered:false,
+      },
+      {
+        question: "49. You feel better",
+        answer: ["A) after making an important decision ", "B) before making an important decision."],answered:false,
+      },
+      {
+        question: "50. When communicating with unfamiliar people, you",
+        answer: ["A) have long conversations ", "B) hardly find topics for conversation"],answered:false,
+      },
+      {
+        question: "51. You trust ",
+        answer: ["A) your experience ", " B) your intuition"],answered:false,
+      },
+      {
+        question: "52. Would you prefer to be considered as",
+        answer: ["A) a practical person ", "B) an inventive person"],answered:false,
+      },
+      {
+        question: "53. More praiseworthy is a man",
+        answer: [" A) who has “clear mind”  ", "B) capable of strong feelings "],answered:false,
+      },
+
+      {
+        question: "54. You are more likely to be",
+        answer: ["A) fair and impartial  ", "B) sympathetic and empathizing"],answered:false,
+      },
+      {
+        question: "55. If you act according to a plan",
+        answer: [" A) you feel comfortable  ", "B) it constrains and limits you "],answered:false,
+      },
+      {
+        question: "56. You do things better when you are",
+        answer: ["A) following a well-thought-out plan  ", "B) coping with surprises"],answered:false,
+      },
+      {
+        question: " 57.Do you think that people close to you have your opinion on certain issues? ",
+        answer: ["A) know your opinion on certain issues, ", "B) will find out only when you tell them . "],answered:false,
+      },
+      {
+        question: "58. In yourself you value",
+        answer: ["A) a strong sense of reality  ", "B) a vivid imagination "],answered:false,
+      },
+
+      {
+        question: "59. If you were a teacher, you would prefer to teach ",
+        answer: ["A) practical subjects ", " B) theoretical subjects "],answered:false,
+      },
+      {
+        question: "60. What do you think is worse - to be ",
+        answer: ["A) too quick-tempered  ", "B) too objective"],answered:false,
+      },
+      {
+        question: "61. You consider yourself as",
+        answer: ["A) sober-minded  ", "B) loyal and responsive "],answered:false,
+      },
+      {
+        question: "62. You are more attracted to situations ",
+        answer: ["A) ordered and planned ", "B) unordered and unplanned "],answered:false,
+      },
+      {
+        question: "63. You are acting ",
+        answer: ["A) more often in accordance to the rules rather than the way you actually want  ", "B) more often the way you actually want rather than in accordance to the rules"],answered:false,
+      },
+      {
+        question: "64. Usually you are",
+        answer: ["A) a sociable person  ", "B) calm and discreet"],answered:false,
+      },
+      {
+        question: "65. When you are writing, you prefer",
+        answer: ["A) writing word for word ", "B)  expressing  figuratively."],answered:false,
+      },
+      {
+        question: "66. You find it harder",
+        answer: ["A) to put yourself in the shoes of other people ", "B) to understand how to get the most benefit from other people."],answered:false,
+      },
+      {
+        question: "67. Would you wish yourself",
+        answer: ["A) a greater mental clarity ", "B) a greater compassion ability"],answered:false,
+      },
+      {
+        question: "68. Would you prefer working with a manager who",
+        answer: ["A) is always fair ", "B) is always benevolent."],answered:false,
+      },
+      {
+        question: "69. Do you prefer",
+        answer: ["A) scheduled events ", "B) spontaneous events"],answered:false,
+      },
+      {
+        question: "70. You are more inclined",
+        answer: ["A) to think and plan ", "B) to act according to circumstances"],answered:false,
+      },
+  ];
+
+ //class for quiz
+  class quiz{
+  	//constructor where I use class functions to generate quiz
+    constructor(Questions){
+      this.arr = Questions;
+      this.displayQuestion();
+      this.generateHtml();
+      this.displayButtons();
+    }
+
+    // function ro generate questions list, and add onlick event for class other function
+    displayQuestion(){
+    // big div for questions
+      let d = document.createElement("div");
+      d.setAttribute("class", "question_big_div");
+      function display(item, i) {
+      let d2 = document.createElement("div");
+      d2.setAttribute("id", i);
+      d2.setAttribute("class", "question_div");
+      d2.setAttribute("onclick", "printQuestion("+i+")");
+      let t = document.createTextNode("question "+(i+1));
+      d2.appendChild(t);
+      d.appendChild(d2);
+     
+        
+  }
+  //adding to body
+  this.arr.forEach(display);  
+  document.body.appendChild(d);
+  
+    }
+   
+  
+ 
+
+    //reload button
+	reload(){
+
+      if (confirm("are you sure that you want to reload?")) {
+        location.reload();
+
+  } 
+    }
+
+    //finish button
+    finish(){
+		let S=1;
+    	Questions.forEach((item, i) => {
+    		
+    		S*=Questions[i].answered;})
+    	if (S == true){
+    	swal("Good job!", "You have answered all the questions", "success");	
+    	}	
+    	else
+	    swal("", "You did not answered  questions", "warning");;
+	
+    }
+
+   //function for generate buttons
+displayButtons(){
+	let main = document.getElementById("main_div");
+	let btn1 = document.createElement("button");
+	let reload = document.createTextNode("reload");
+	btn1.appendChild(reload);
+	btn1.setAttribute("id", "reload");
+	btn1.setAttribute("class", "button");
+	let btn2 = document.createElement("button");
+	let finish = document.createTextNode("finish");
+	btn2.appendChild(finish);
+    btn2.setAttribute("id", "finish");
+    btn2.setAttribute("class", "button");
+	main.appendChild(btn1);
+	main.appendChild(btn2);
+	document.getElementById("reload").addEventListener('click', this.reload);
+    document.getElementById("finish").addEventListener('click', this.finish);
+}
+
+
+
+
+// generate main html for quiz
+generateHtml(){
+	let main = document.createElement("div");
+	main.setAttribute("id", "main_div");
+	main.setAttribute("class", "main_div");
+  	let question = document.createElement("p");
+  	let text = document.createTextNode("choose question");
+  	question.setAttribute("id", "question");
+  	question.appendChild(text);
+  	main.appendChild(question);
+  	document.body.appendChild(main);
+}
+}
+//I placed these functions outside the class because I think its more universal and compact
+//let tmp = [tmp1="",tmp2=""];
+
+ //function to print questions and answers for user
+function printQuestion(a){
+    document.getElementById("question").innerHTML = "<div>"+Questions[a].question+
+    "</div><div style='cursor: pointer'><input type='radio' name='answer' onchange='answered("+a+")'>"
+    +Questions[a].answer[0]+"<br<<br><input type='radio' name='answer' onchange='answered("+a+")'>"
+    +Questions[a].answer[1]+"</div>";
+    btnColor(a);
+  	}
+
+function ckeck(item, i){
+
+	if(Questions[i].answered == false){
+		
+    document.getElementById(i).style.backgroundColor="rgba(255, 255, 255, 0.8)";
+    document.getElementById(i).style.color="#3933A6";
+      }
+if(Questions[i].answered == true){
+     document.getElementById(i).style.backgroundColor="#568203";
+     document.getElementById(i).style.color="#FEFEFA";
+      }
+}
+
+function btnColor(a) {
+	//tmp.tmp1= document.getElementById(a).id
+   // Questions[a].answered = false;
+	
+    
+Questions.forEach(ckeck);
+    document.getElementById(a).style.backgroundColor="yellow";
+    document.getElementById(a).style.color="#3933A6";
+    
+
+}
+
+//function in case the question is answrer
+function answered(a){
+ document.getElementById(a).style.backgroundColor="#568203";
+ document.getElementById(a).style.color="#FEFEFA";
+ Questions[a].answered = true;
+
+}
+
+
+//initializing quiz class object
+let test= new quiz(Questions);
+
+
+  
